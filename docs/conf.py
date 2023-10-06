b8df39e480c7e58e97ab47c917312a1b46c58069
Morten Sahlertz <mortenjordanthebest@hotmail.com>
2023-10-06 12:11:50 +0200

    ...

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,13 +24,15 @@ author = 'Morten Sahlertz'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.viewcode', 'sphinx.ext.autodoc'
+extensions = [
+'sphinx.ext.viewcode',
+'sphinx.ext.autodoc',
+'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/index.rst
+++ docs/index.rst
@@ -10,7 +10,7 @@ Welcome to krippendorff_alpha's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
+   modules
 
 Indices and tables
 ==================
